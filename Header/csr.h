04fc237b41ffaf537115356663b35fd0112d5851
mikigol <golemi.17mikele@gmail.com>
2025-11-07 19:28:22 +0100

    Update csr.h

--- Header/csr.h
+++ Header/csr.h
@@ -14,7 +14,8 @@
 void csr_spmv_seq(Matrix *mat, double *x, double *y);
 
 // Moltiplicazione CSR parallela
-void csr_spmv_parallel(Matrix *mat, double *x, double *y, int num_threads);
+void csr_spmv_parallel_schedule(Matrix *mat, double *x, double *y, 
+                                  int num_threads, int schedule_type, int chunk_size);
 
 #endif
 
