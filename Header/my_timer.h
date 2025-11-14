e2d22f6ca59bdb37675d75834a75f60bcaa629b1
mikigol <golemi.17mikele@gmail.com>
2025-11-14 17:58:45 +0100

    Delete Header directory

--- Header/my_timer.h
+++
@@ -1,8 +0,0 @@
-#include <sys/time.h>
-
-#define GET_TIME(now) {                  \
-   struct timeval t;                     \
-   gettimeofday(&t, NULL);               \
-   now = t.tv_sec + t.tv_usec/1000000.0; \
-}
-
