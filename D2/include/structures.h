e1e57d3b595df5c70b102a572c32ea1cfcf90e59
mikigol <golemi.17mikele@gmail.com>
2026-02-04 20:51:15 +0100

    Update structures.h

--- D2/include/structures.h
+++ D2/include/structures.h
@@ -3,11 +3,9 @@
 
 #include <mpi.h>
 
-// Macro per distribuzione ciclica (usate ovunque)
 #define GET_OWNER(glob_idx, size) ((glob_idx) % (size))
 #define GET_LOCAL_IDX(glob_idx, size) ((glob_idx) / (size))
 
-// Struttura per la matrice CSR locale
 typedef struct {
     int n_local_rows;
     int n_local_nz;
@@ -16,23 +14,19 @@ typedef struct {
     double *val;
 } LocalCSR;
 
-// Struttura per gestire la comunicazione (Ghost exchange)
 typedef struct {
     int num_ghosts;
     int total_to_send;
     
-    // Buffer
     double *send_buffer;
     double *recv_buffer;
     
-    // MPI Arrays per Alltoallv
     int *send_counts;
     int *recv_counts;
     int *sdispls;
     int *rdispls;
     
-    // Indici per mappare i dati
-    int *export_indices;     // Quali miei elementi devo copiare nel buffer di invio
+    int *export_indices;     
 } CommInfo;
 
 void free_local_csr(LocalCSR *mat);
